e3b7a4b9d3097df730d3d55af01c911b87498fce
LindaPetrini <linda.petrini@student.uva.nl>
2017-11-22 16:36:56 +0100

    initial commit

---
+++ experiments/test_3.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.nn import init
+from torch.nn._functions.rnn import Recurrent, StackedRNN
+import os
+import torchwordemb
+
+
+class CNN(nn.Module):
+    def __init__(self, emb_size, pad_len, classes):
+        super(CNN, self).__init__()
+        self.emb_size = emb_size
+        self.pad_len = pad_len
+        self.classes = classes
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+
+        self.fc = nn.Linear(16*int(self.emb_size/2)*int(self.pad_len/2), self.classes)
+        self.sm = nn.Softmax()
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        out = self.sm(out)
+        return out
+
+
+
+# class RAN(nn.Module):
+#
+#     def __init__(self, input_size, hidden_size, nlayers=1, dropout=0.5):
+#         super().__init__()
+#         if nlayers > 1:
+#             raise NotImplementedError("TODO: nlayers > 1")
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.nlayers = nlayers
+#         self.dropout = dropout
+#
+#         self.w_cx = nn.Parameter(torch.Tensor(hidden_size, input_size))
+#         self.w_ic = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
+#         self.w_ix = nn.Parameter(torch.Tensor(hidden_size, input_size))
+#         self.w_fc = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
+#         self.w_fx = nn.Parameter(torch.Tensor(hidden_size, input_size))
+#
+#         self.b_cx = nn.Parameter(torch.Tensor(hidden_size))
+#         self.b_ic = nn.Parameter(torch.Tensor(hidden_size))
+#         self.b_ix = nn.Parameter(torch.Tensor(hidden_size))
+#         self.b_fc = nn.Parameter(torch.Tensor(hidden_size))
+#         self.b_fx = nn.Parameter(torch.Tensor(hidden_size))
+#
+#         self.weights = self.w_cx, self.w_ic, self.w_ix, self.w_fc, self.w_fx
+#         for w in self.weights:
+#             init.xavier_uniform(w)
+#
+#         self.biases = self.b_cx, self.b_ic, self.b_ix, self.b_fc, self.b_fx
+#         for b in self.biases:
+#             b.data.fill_(0)
+#
+#     def forward(self, input, hidden):
+#         layer = (Recurrent(RANCell), )
+#         func = StackedRNN(layer, self.nlayers, dropout=self.dropout)
+#         hidden, output = func(input, hidden, ((self.weights, self.biases), ))
+#         return output, hidden
+#
+#
+# def RANCell(input, hidden, weights, biases):
+#     w_cx, w_ic, w_ix, w_fc, w_fx = weights
+#     b_cx, b_ic, b_ix, b_fc, b_fx = biases
+#
+#     ctilde_t = F.linear(input, w_cx, b_cx)
+#     i_t = F.sigmoid(F.linear(hidden, w_ic, b_ic) + F.linear(input, w_ix, b_ix))
+#     f_t = F.sigmoid(F.linear(hidden, w_fc, b_fc) + F.linear(input, w_fx, b_fx))
+#     c_t = i_t * ctilde_t + f_t * hidden
+#     h_t = F.tanh(c_t)
+#
+#     return h_t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ experiments/test__2.py
@@ -0,0 +1,114 @@
+import os
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.autograd as autograd
+import torch.nn.functional as F
+from torch.nn import init
+from torch.nn._functions.rnn import Recurrent, StackedRNN
+import torchwordemb
+from test_3 import CNN
+import numpy as np
+
+
+#vocab, vec = torchwordemb.load_word2vec_bin("./GoogleNews-vectors-negative300.bin")
+
+train_data = []
+train_targets = []
+
+fname = './2017_English_final/GOLD/Subtask_A/twitter-2016train-A.txt'
+something = True
+for line in open(fname):
+    if something:
+        something = False
+    else:
+        splitted_line = line.split()
+        train_targets.append(1 if splitted_line[1]=="positive" else 0 if splitted_line[1]=="neutral" else -1)
+        train_data.append(splitted_line[2:])
+
+punct = [".", ",", ";", ":", '"', "@", "#"]
+for ind, sentence in enumerate(train_data):
+
+    a = ["".join([letter if letter not in punct else "" for letter in word.lower()])
+         if "http" not in word else None for word in sentence]
+    if None in a:
+        a.remove(None)
+    train_data[ind] = a
+
+pad_len = len(max(train_data, key=lambda x: len(x)))
+print(pad_len)
+
+
+
+for sentence in train_data:
+    if len(sentence) < pad_len:
+        tmp = pad_len - len(sentence)
+        for i in range(tmp):
+            sentence.append("<hey>")
+
+
+vocab, vec = {}, {}
+
+
+batch_size = 10
+window = 3
+emb_size = 300
+classes = 3
+
+for ind, sentence in enumerate(train_data):
+    for k, word in enumerate(sentence):
+        if word not in vocab:
+            vocab[word] = len(vocab)
+            vec[vocab[word]] = np.random.normal(0, 1, emb_size)
+
+
+cnn = CNN(emb_size, pad_len, classes)
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=0.01)
+loss = nn.NLLLoss()
+
+
+
+for ind, sentence in enumerate(train_data):
+    emb_mat = np.zeros((emb_size, pad_len))
+    for k, word in enumerate(sentence):
+        if word in vocab:
+            emb_mat[:, k] = vec[vocab[word]]
+
+        else:
+            emb_mat[:, k] = np.random.normal(0, 1, emb_size)
+
+    inp = Variable(torch.Tensor(emb_mat).view(1, 1, emb_size, -1))
+    out = cnn.forward(inp)
+    print("\ntweet ", ind)
+    expected_targ = Variable(torch.LongTensor([train_targets[ind]+1]))
+    print(expected_targ)
+    print(out)
+    optimizer.zero_grad()
+    error = loss(out, expected_targ)
+
+
+    error.backward()
+    optimizer.step()
+    print("error ", error)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
