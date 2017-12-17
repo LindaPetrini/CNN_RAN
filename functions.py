813013ceb077869060bf2a78235c6115bd998d10
Davide Belli <davidebelli95@gmail.com>
2017-12-17 20:28:33 +0100

    commit backup

--- functions.py
+++ functions.py
@@ -1,5 +1,3 @@
-from nltk.tokenize import TweetTokenizer
-import re
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
@@ -8,58 +6,93 @@ import numpy as np
 import gc
 
 
-
-
 class CNN(nn.Module):
-    def __init__(self, emb_size, pad_len, classes, vocab_size):
+    def __init__(self, emb_size, pad_len, batch_size, classes, vocab_size):
         super(CNN, self).__init__()
-
+        
+        self.filters_sizes = [3, 4, 5]
+        self.channels = 10
+        
+        
+        self.batch_size = batch_size
         self.emb_size = emb_size
         self.pad_len = pad_len
         self.classes = classes
         self.vocab_size = vocab_size
-        self.window = 3
-        self.channels = 16
-        self.embeddings = nn.Embedding(self.vocab_size + 1, self.emb_size)
+        
+        
+        self.encoder = nn.Embedding(self.vocab_size, self.emb_size, padding_idx=0)
+        
+        
+        conv_layers = []
+        for i, filter in enumerate(self.filters_sizes):
+            conv_layers.append(
+                nn.Sequential(
+                    nn.Conv2d(1, self.channels, kernel_size=(self.emb_size, filter), padding=0),
+                    nn.ReLU(),
+                    nn.MaxPool2d((1, self.pad_len -filter +1))
+                )
+            )
 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, self.channels, kernel_size=(self.emb_size, self.window), padding=1),
-            nn.BatchNorm2d(self.channels),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
+        self.conv_layers = nn.ModuleList(conv_layers)
 
-        self.fc = nn.Linear(self.channels*int(self.pad_len/2), self.classes)
+        self.do = nn.Dropout(0.5)
+        
+        self.fc = nn.Linear(self.channels*len(self.filters_sizes), self.classes)
+        
         self.sm = nn.LogSoftmax()
 
     def forward(self, x):
-        x = self.embeddings(x).view((1, 1, self.emb_size, -1))
-        out = self.layer1(x)
-        out = out.view(out.size(0), -1)
+        x = self.encoder(x).view((self.batch_size, 1, self.emb_size, -1))
+        
+        features = []
+        
+        for conv in self.conv_layers:
+            c = conv(x)
+            features.append(c.view(c.size(0), -1))
+        
+            
+        out = torch.cat(features, dim=1)
+        
+        #out = out.view(out.size(0), -1)
+        
+        out = self.do(out)
+        
         out = self.fc(out)
 
         out = self.sm(out)
+        
         return out
 
+
+    
     def init_emb(self, vocaboulary):
         emb_mat = self.create_emb_matrix(vocaboulary)
-        self.embeddings.weight.data.copy_(torch.from_numpy(emb_mat))
+        self.encoder.weight.data.copy_(torch.from_numpy(emb_mat))
 
+    def init_from_txt(self, fname, vocaboulary):
+        emb_mat =  np.zeros((self.vocab_size, self.emb_size))
+        with open(fname, 'r', encoding='utf-8') as f:
+            for l in f:
+                word, vec = l.split("\t")
+                vec = vec.split(",")
+                emb_mat[vocaboulary[word]] = np.asarray(vec)
+        self.encoder.weight.data.copy_(torch.from_numpy(emb_mat))
 
     def create_emb_matrix(self, vocaboulary):
         print('importing embeddings')
-        vocab, vec = torchwordemb.load_word2vec_bin("./GoogleNews-vectors-negative300.bin")
+        vocab, vec = torchwordemb.load_word2vec_bin("./datasets/GoogleNews-vectors-negative300.bin")
         print('imported embeddings')
 
-        emb_mat = np.zeros((self.vocab_size + 1, self.emb_size))
+        emb_mat = np.zeros((self.vocab_size, self.emb_size))
 
-        for word in vocaboulary.keys():
+        for i, word in enumerate(vocaboulary.keys()):
+            if i % 1000 == 0:
+                print("Reading word ", i, "/", len(vocaboulary))
             if word in vocab:
                 emb_mat[vocaboulary[word]] = vec[vocab[word]].numpy()
             else:
                 emb_mat[vocaboulary[word]] = np.random.normal(0, 1, self.emb_size)
-
-        #hypotetically, the one for <unk>
-        emb_mat[-1] = np.random.normal(0, 1, self.emb_size)
 
         print('train matrices built')
 
@@ -70,89 +103,25 @@ class CNN(nn.Module):
         print('garbage collected')
 
         return emb_mat
+    
 
-    def encode_words(self, sentence, word2ind):
+    def encode_words(self, sentence, word2ind, is_cuda=True):
         inp = np.asarray([word2ind[word] for word in sentence])
-        #inp.reshape((1, 1, self.pad_len))
-        inp = Variable(torch.LongTensor(inp))
+        inp = Variable(torch.cuda.LongTensor(inp.tolist())) if is_cuda else Variable(torch.LongTensor(inp))
         return inp
 
+    def emb_to_txt(self, oname: str, word2ind: dict):
+    
+        embeddings = self.encoder.weight.data.cpu().numpy()
+        with open(oname, "w") as o:
+            o.write("#word\tembedding\n")
+            for word in word2ind.keys():
+                emb = embeddings[word2ind[word]]
+                emb = [str(el) for el in emb]
+                o.write(str(word) + "\t" + ", ".join(emb) + "\n")
 
 
 
 
 
-
-
-
-def read_data(fname, pad_len=50, padding=True):
-    train_data = []
-    train_targets = []
-
-    with open(fname) as f:
-        f.readline()
-        for line in f.readlines():
-            train_data.append(line)
-            line = line.strip().split()
-            if line[1] == 'positive':
-                train_targets.append(2)
-            elif line[1] == 'negative':
-                train_targets.append(0)
-            else:
-                train_targets.append(1)
-
-    tknzr = TweetTokenizer()
-    max_len = 0
-
-    for ind, sentence in enumerate(train_data):
-        tmp = sentence.strip().split(None, 2)[2]
-        tmp = re.sub("https?:?//[\w/.]+", "<URL>", tmp)
-        tmp = find_emoticons(tmp)
-        tmp = re.sub("[\-_#@':.]", " ", tmp)
-        train_data[ind] = tknzr.tokenize(tmp.lower())
-        max_len = max(max_len, len(train_data[ind]))
-
-    actual_pad = max(max_len, pad_len)
-    if padding:
-        for sentence in train_data:
-            assert len(sentence) <= actual_pad, "tweet longer than padding"
-
-            while len(sentence) < actual_pad:
-                sentence.append("<PAD>")
-
-    return train_data, train_targets, actual_pad
-
-
-def find_emoticons(sentence):
-    smile_emoticons_str = r"""
-        (?:
-            [:=;] # Eyes
-            [oO\-]? # Nose (optional)
-            [D\)\]] # Mouth
-        )"""
-
-    sad_emoticons_str = r"""
-        (?:
-            [:=;] # Eyes
-            [oO\-']? # Nose (optional)
-            [\(\[\|] # Mouth
-        )"""
-    funny_emoticons_str = r"""
-        (?:
-            [:=;] # Eyes
-            [oO\-]? # Nose (optional)
-            [OpP] # Mouth
-        )"""
-    sentence = re.sub(smile_emoticons_str, "<smile>", sentence)
-    sentence = re.sub(sad_emoticons_str, "<sad>", sentence)
-    sentence = re.sub(funny_emoticons_str, "<funny>", sentence)
-
-    return sentence
-
-def create_vocab(vocab_list):
-    vocab = {}
-    for word in vocab_list:
-        if word not in vocab:
-            vocab[word] = len(vocab)
-    return vocab
 
--- main.py
+++ main.py
@@ -1,86 +1,297 @@
 import functions as fun
-import torch
+import batchifier
+import data
+import argparse
+import time
 import torch.nn as nn
-from torch.autograd import Variable
+import torch.optim
 import numpy as np
-from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
-import pprint
+import os.path
+
+parser = argparse.ArgumentParser(description='CNN')
+parser.add_argument('--initial', type=str, default=None,
+                    help='choose initialisation(google, path_to_file, None = random)')
+parser.add_argument('--epochs', type=int, default=10,
+                    help='number of epochs')
+parser.add_argument('--lr', type=float, default=0.001,
+                    help='learning rate')
+parser.add_argument('--plot', action='store_true',
+                    help='plot loss')
+parser.add_argument('--save', type=str, default='./embeddings/'+str(time.time())+'/trained_emb.txt',
+                    help='path to save the final model')
+parser.add_argument('--dataset', type=str, default='./datasets/preprocessed.txt',
+                    help='path to the dataset')
+parser.add_argument('--cuda', action='store_true',
+                    help='use CUDA')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--shuffle', action='store_true',
+                    help='shuffle train data every epoch')
+parser.add_argument('--batch_size', type=int, default=200, metavar='N',
+                    help='batch size')
+parser.add_argument('--pause', action='store_true',
+                    help='not optimise embeddings for the first 5 epochs')
+parser.add_argument('--pause_value', type=int, default=0,
+                    help='not optimise embeddings for the first 5 epochs')
+parser.add_argument('--log_interval', type=int, default=100, metavar='N',
+                    help='report interval')
+parser.add_argument('--lim', type=int, default=float('+inf'), help='set maximum number of datapoints to use')
+
+
+args = parser.parse_args()
+
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    else:
+        torch.cuda.manual_seed(args.seed)
+
+
+# def read_data(fname):
+#     train_data = []
+#     train_targets = []
+#
+#     with open(fname) as f:
+#         f.readline()
+#         for line in f.readlines():
+#             train_target, train_sentence = line.strip().split(None, 1)
+#             train_data.append(train_sentence.split(" "))
+#             train_targets.append(int(train_target))
+#
+#     tweet_len = len(max(train_data, key=lambda x: len(x)))
+#
+#     print('Dataset size:', len(train_data))
+#
+#     return train_data, train_targets, tweet_len
+
+# def create_vocab(vocab_list):
+#     vocab = {}
+#     for word in vocab_list:
+#         if word not in vocab:
+#             vocab[word] = len(vocab)
+#     return vocab
 
 
 
+print("Reading data...")
+#train_data, train_targets, pad_len = read_data(args.dataset)
+
+corpus = data.Corpus(args.dataset, cuda=args.cuda, lim=args.lim)
+
+#train_data, train_targets, pad_len = corpus.data, corpus.target, corpus.tweet_len
+# print("\nnumber train data: ", len(train_data))
+# print("padding length: ", pad_len)
+
+print("\nnumber train data: ", len(corpus.data))
+print("padding length: ", corpus.tweet_len)
 
 
-
-
-train_fname = './2017_English_final/GOLD/Subtask_A/twitter-2016train-A.txt'
-test_fname = './2017_English_final/GOLD/Subtask_A/twitter-2016test-A.txt'
-
-print("reading data")
-train_data, train_targets, pad_len = fun.read_data(train_fname)
-#test_data, test_targets, test_pad_len = fun.read_data(test_fname)
-
-#assert pad_len == test_pad_len, "tweet length not matching"
-
-print("\nnumber train data: ", len(train_data))
-#print("number test data: ", len(test_data))
-print("padding length: ", pad_len)
-
+eval_batch_size = 10
 emb_size = 300
-classes = 3
+classes = 2
+
+train_data = batchifier.batchify(corpus.data, args.batch_size, args.cuda)
+train_data_t = batchifier.batchify_target(corpus.target, args.batch_size, classes, args.cuda)
+
+train_confusion = np.reshape([[0 for i in range(classes)] for j in range(classes)], (classes, classes))
 
 
-unique_words = [word for sentence in train_data for word in sentence]
-unique_words = list(set(unique_words))
-vocab_size = len(unique_words)
+cnn = fun.CNN(emb_size, corpus.tweet_len, args.batch_size, classes, len(corpus.dictionary.word2idx))
 
-word2ind = fun.create_vocab(unique_words)
-pprint.pprint(locals())
-cnn = fun.CNN(emb_size, pad_len, classes, vocab_size)
+lossCriterion = nn.NLLLoss()
 
-cnn.init_emb(word2ind)
+if args.cuda:
+    cnn.cuda()
 
-optimizer = torch.optim.Adam(cnn.parameters(), lr=0.001)
+if args.save:
+    dir = os.path.dirname(args.save)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-loss = nn.NLLLoss()
+if args.initial == "google":
+    cnn.init_emb(corpus.dictionary.word2idx)
+elif args.initial is not None:
+    if os.path.exists(args.initial):
+        cnn.init_from_txt(args.initial, corpus.dictionary.word2idx)
+    else:
+        raise FileNotFoundError("File {} doesn't exist".format(args.initial))
 
 
-epochs = 10
-errors = []
 
-for it in range(epochs):
-    train_data, train_targets = shuffle(train_data, train_targets)
 
-    print("****************************************************************************")
-    print("iter = ", it + 1)
-    print("****************************************************************************")
-    total_err = 0
 
-    for index, sentence in enumerate(train_data):
+lr = args.lr
+best_val_loss = None
+best_epoch = -1
+best_recall_epoch = -1
+best_fitness = 0
 
-        inp = cnn.encode_words(sentence, word2ind)
-        out = cnn.forward(inp)
-        target = train_targets[index]
-        expected_targ = Variable(torch.LongTensor([target]))
+if args.pause:
+    optimizer = torch.optim.Adagrad(filter(lambda p: p.requires_grad, cnn.parameters()), lr=args.lr)
+else:
+    optimizer = torch.optim.Adagrad(cnn.parameters(), lr=args.lr)
 
+def confusion_matrix(output, target, matrix, n_classes):
+    _, y = torch.max(output.view(-1, n_classes), 1)
+    _, t = torch.max(target.view(-1, n_classes), 1)
+    t = t.data.cpu().numpy()
+    y = y.data.cpu().numpy()
+    assert len(t) == len(y), "target and output have different sizes"
+    for i in range(len(t)):
+        matrix[t[i], y[i]] += 1
+    return
+
+def recallFitness(conf_arr, n_classes):
+    recall = np.zeros(n_classes)
+    for i in range(len(conf_arr[0])):
+        recall[i] = conf_arr[i, i] / (np.sum(conf_arr[i]))
+    average_recall = np.sum(recall) / n_classes
+    return average_recall
+
+def train():
+    # Turn on training mode which enables dropout.
+    cnn.train()
+    
+    epoch_loss = 0
+    total_loss = 0
+    
+    start_time = time.time()
+    
+    for batch, i in enumerate(range(0, train_data.size(0)-1, corpus.tweet_len)):
+        # print("training........... ", train_data.size(0)," ", corpus.tweet_len)
         optimizer.zero_grad()
-
-        error = loss(out, expected_targ)
-        error.backward()
+        data, targets = batchifier.get_batch(train_data, train_data_t, i, corpus.tweet_len, corpus.n_classes)
+        
+        #print('batch {}: {}'.format(batch+1, data.size()))
+        targets = targets[-1]
+        
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        
+        cnn.zero_grad()
+        
+        output = cnn(data)
+        
+        _, index_target = torch.max(targets, 1)
+        loss = lossCriterion(output.view(-1, corpus.n_classes), index_target.view(-1))
+        loss.backward()
+        
         optimizer.step()
-        total_err += error
-        if index % 200 == 0:
-            print('\tExpected - Predicted:', target, np.argmax(out.data.numpy().flatten()))
-            print("tweet ", index)
-            print("\tError ", error.data.numpy().flatten())
-    errors.append(total_err / len(train_data))
-    print("Errors: ", errors)
 
-print("errors:", errors)
-plt.plot(range(epochs), errors)
-plt.show()
+        total_loss += loss.data[0]
+        epoch_loss += loss.data[0]
 
-torch.save(cnn, 'cnn.pt')
+        confusion_matrix(output, targets, train_confusion, corpus.n_classes)
+        
+        if batch % args.log_interval == 0:
+            cur_loss = total_loss# / args.log_interval
+            cur_recall = recallFitness(train_confusion, corpus.n_classes) #/ args.log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:2d}| {:3d}/{:3d}| ms/btc {:4.2f}| '
+                  'loss {:5.2f} | Rec {:3.4f} '.format(
+                epoch, batch+1, len(train_data) // corpus.tweet_len,
+                              elapsed * 1000 / args.log_interval, cur_loss, cur_recall))
+            total_loss = 0
+            start_time = time.time()
+        
+        
+    
+    return epoch_loss * args.batch_size / train_data.size(0)
 
+def plotter(conf_arr, epoch=0):
+    fig = plt.figure()
+    plt.clf()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    res = ax.imshow(np.array(conf_arr), cmap=plt.cm.jet,
+                    interpolation='nearest')
+    
+    # print(conf_arr)
+    width, height = conf_arr.shape
 
+    for x in range(width):
+        for y in range(height):
+            ax.annotate(str(conf_arr[x][y]), xy=(y, x),
+                        horizontalalignment='center',
+                        verticalalignment='center')
 
+    cb = fig.colorbar(res)
+    alphabet = ["negative", "positive"]
+    plt.xticks(range(width), alphabet[:width])
+    plt.yticks(range(height), alphabet[:height])
+    if not os.path.exists(path):
+        os.makedirs(path)
+    plt.savefig(path + "confusion_matrix_" + str(epoch) + '.png', format='png')
+    plt.close()
+    return
+
+print("START TRAINING")
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+    exec_time = time.time()
+    path = "./confusion_matrixes/" +\
+           "_lr" + str(args.lr) +\
+           "_batchsize_" + str(args.batch_size) +\
+           "_" + str(exec_time)[-3:] +\
+           ("_pause" if args.pause else "") + \
+           ("_google" if args.initial == 'google' else "prev" if args.initial else "") + \
+           ("_shuffle/" if args.shuffle else "/")
+    
+    begin_time = time.time()
+    
+    losses = []
+    
+    for epoch in range(1, args.epochs + 1):
+        if args.pause:
+            if epoch > args.pause_value:
+                cnn.encoder.weight.requires_grad = True
+                optimizer = torch.optim.Adagrad(cnn.parameters(), lr=args.lr)
+        epoch_start_time = time.time()
+        
+        #if args.shuffle:
+            # print("...shuffling")
+            #train_data, train_data_t = batchifier.shuffle_data(corpus, epoch, args.batch_size, corpus.n_classes, args.cuda)
+            # print("...shuffled!")
+        
+        train_confusion = np.reshape([[0 for i in range(classes)] for j in range(classes)], (classes, classes))
+        loss = train()
+        
+        losses.append(loss)
+
+        elapsed = time.time() - epoch_start_time
+        
+        print('| epoch {:2d}| train loss: {:4f} | s {:3.3f}| '.format(epoch, loss, elapsed))
+        if args.plot:
+            plotter(train_confusion, epoch)
+
+        if args.save:
+            name, ext = os.path.splitext(args.save)
+            cnn.emb_to_txt(name+str(epoch)+ext, corpus.dictionary.word2idx)
+        
+
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
+
+end_time = time.time()
+
+print('Total Execution Time:', end_time - begin_time)
+
+print(train_confusion)
+if args.plot:
+    plt.plot(range(len(losses)), losses)
+    plt.savefig(
+                os.path.dirname(args.save)+
+                "_losstrend" +
+                "_lr" + str(args.lr) +
+                "_btchsize_" + str(args.batch_size) +
+                "_" + str(exec_time)[-3:] +
+                ("_pause" if args.pause else "") +
+                ("_google" if args.initial == 'google' else "prev" if args.initial else "") + \
+                ("_shuffle" if args.shuffle else "") +
+                '.png'
+                )
+
+if args.save:
+    cnn.emb_to_txt(args.save, corpus.dictionary.word2idx)
