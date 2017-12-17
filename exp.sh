69e67d9527c437e30fe208153385360231af7432
Davide Belli <davidebelli95@gmail.com>
2017-12-17 20:23:54 +0100

    removed old stuff

--- exp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python main.py --cuda --plot --shuffle --initial google --pause_value 1 --epochs 50 --lim 1000 --lr 0.005 --batch_size 100
-python main.py --cuda --plot --shuffle --initial google --pause_value 1 --epochs 50 --lim 1000 --lr 0.001 --batch_size 200
-
-python main.py --cuda --plot --shuffle --initial google --pause_value 1 --epochs 50 --lim 10000 --lr 0.001 --batch_size 100
-python main.py --cuda --plot --shuffle --initial google --pause_value 1 --epochs 50 --lim 10000 --lr 0.001 --batch_size 200
-python main.py --cuda --plot --shuffle --initial google --pause_value 1 --epochs 50 --lim 10000 --lr 0.001 --batch_size 400
